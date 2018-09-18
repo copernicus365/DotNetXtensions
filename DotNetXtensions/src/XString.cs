d41e5ceb73c7af21608f81ae0880eb8ab6d773b2
Nicholas Petersen <copernicus365@gmail.com>
2018-09-18 18:56:12 -0400

    --- XString breaking changes (2.0.0) ---
    Documentation added to:
     XString.CharFromEnd,
     XString.CutEnd
    
    Documentation reworked:
     - XString.End
    
    API change!:
    
    1) `CutEnd` overload that took param `string endString` renamed as follows:
    
    `string CutEnd(this string s, string endString, bool checkDoesEndWith = true, bool ignoreCase = false)`
    
    renamed to:
    
    `string CutEndIfEndsWith(this string s, string endString, bool checkDoesEndWith = true, bool ignoreCase = false)`
    
    2) `CutEnd` overload that took param `char endChar` renamed as follows:
    
    `string CutEnd(this string s, char endChar, bool ignoreCase = false)`
    
    renamed to:
    
    `string CutLastCharIfEndsWith(this string s, char endChar, bool ignoreCase = false)`

--- DotNetXtensions/src/XString.cs
+++ DotNetXtensions/src/XString.cs
@@ -481,6 +481,19 @@ namespace DotNetXtensionsPrivate
 			return idx + value.Length;
 		}
 
+
+
+		#region --- End and Cut End Functions ---
+
+		/// <summary>
+		/// Gets the char from teh specified count *from the end* of the string.
+		/// Returns <see cref="char.MinValue"/> if index was outside of range 
+		/// of the string.
+		/// </summary>
+		/// <param name="s">String</param>
+		/// <param name="indexFromEnd">Index from end of string to get the char from.
+		/// Consider the last char index '0', the second to last index '1', etc.</param>
+		/// <returns></returns>
 		public static char CharFromEnd(this string s, int indexFromEnd)
 		{
 			int len = s?.Length ?? -1;
@@ -500,6 +513,14 @@ namespace DotNetXtensionsPrivate
 			return val;
 		}
 
+		/// <summary>
+		/// Removes the specified length of chars from the end of source string 
+		/// and returns the substring result. Gracefully handles if the cut length is 
+		/// greater than or equal to the source string, in which case returns an empty 
+		/// string.
+		/// </summary>
+		/// <param name="s">String</param>
+		/// <param name="cutFromEnd">Count of chars to remove (cut) from the end of source string</param>
 		public static string CutEnd(this string s, int cutFromEnd)
 		{
 			if (s.IsNulle() || cutFromEnd < 0) return s;
@@ -511,18 +532,20 @@ namespace DotNetXtensionsPrivate
 		}
 
 		/// <summary>
-		/// Cuts the source string by the length of the specified endString.
-		/// By default checks first that the source string does end with that value
-		/// (if not, returns source string). Gracefully ignores any null or empty
-		/// strings (by returning source string), or when end is longer than source (by returning empty).
+		/// Removes the final substring from source string *if* it actually ends with the specified
+		/// <paramref name="endString"/>. Else returns full string. If <paramref name="checkDoesEndWith"/>
+		/// is true, then no check is even done to see that it actually ends with <paramref name="endString"/>.
+		/// This is useful when one has already checked, so the extra check does not have to be
+		/// reperformed. Gracefully ignores any null or empty strings (by returning source string), 
+		/// or when end is longer than source (by returning empty).
 		/// </summary>
 		/// <param name="s">Source string.</param>
 		/// <param name="endString">End string expected to occur at end of source string. 
 		/// If not, will return source string.</param>
 		/// <param name="checkDoesEndWith">True (by default) to check the end does actually end
 		/// the source. Specify false to ignore this check if you already know it is a match.</param>
-		/// <param name="ignoreCase">True to have the end check does case-insensitive (OrdinalIgnoreCase).</param>
-		public static string CutEnd(this string s, string endString, bool checkDoesEndWith = true, bool ignoreCase = false)
+		/// <param name="ignoreCase">True to have the end check done case-insensitive (OrdinalIgnoreCase).</param>
+		public static string CutEndIfEndsWith(this string s, string endString, bool checkDoesEndWith = true, bool ignoreCase = false)
 		{
 			if (s.IsNulle() || endString.IsNulle())
 				return s;
@@ -535,7 +558,14 @@ namespace DotNetXtensionsPrivate
 			return s.CutEnd(endString.Length);
 		}
 
-		public static string CutEnd(this string s, char endChar, bool ignoreCase = false)
+		/// <summary>
+		/// Removes the final character from string *if* it ends with the specified
+		/// <paramref name="endChar"/>. Else returns full string.
+		/// </summary>
+		/// <param name="s">String</param>
+		/// <param name="endChar">End character to remove if it exists as last char.</param>
+		/// <param name="ignoreCase">True to ignore case.</param>
+		public static string CutLastCharIfEndsWith(this string s, char endChar, bool ignoreCase = false)
 		{
 			if (s.IsNulle())
 				return s;
@@ -553,22 +583,13 @@ namespace DotNetXtensionsPrivate
 			return s.Substring(0, s.Length - 1);
 		}
 
-		///// <summary>
-		///// Finds the last index in string of <paramref name="cutAtValue"/>
-		///// and returns a substring which is cut off at that point. If the value is never found,
-		///// source string is returned. Often <paramref name="cutAtValue"/> will simply be 
-		///// the very end of the source string (e.g. <c>CutEnd("file.txt", ".txt")</c> returns <c>"file"</c>),
-		///// but it can be anywhere in the string.
-		///// </summary>
-		///// <param name="s">Source string.</param>
-		///// <param name="cutAtValue">The string to find the last instance of this value
-		///// in the source string (may be the very end of the string).</param>
-		//public static string CutEnd(this string s, string cutAtValue)
-		//{
-		//	if(s.IsNulle() || cutAtValue.IsNulle()) return s;
-		//	return CutEnd(s, s.LastIndexOf(cutAtValue));
-		//}
-
+		/// <summary>
+		/// Gets the end substring of the source string by the specified length.
+		/// *NO* exception is thrown if the length was longer than string, 
+		/// just returns full string then.
+		/// </summary>
+		/// <param name="s">String</param>
+		/// <param name="length">Count of characters from end to get.</param>
 		public static string End(this string s, int length)
 		{
 			if (s == null || s.Length <= length || length < 1)
@@ -576,6 +597,10 @@ namespace DotNetXtensionsPrivate
 
 			return s.Substring(s.Length - length, length);
 		}
+
+		#endregion
+
+
 
 		public static bool PreviousIndexIsMatch(this string s, int idx, Func<char, bool?> matchOrBreakOrContinuePredicate)
 		{

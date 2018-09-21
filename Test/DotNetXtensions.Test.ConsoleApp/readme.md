bb3d7607bd47f250dba5ded572a6390999bc46a2
Nicholas Petersen <copernicus365@gmail.com>
2018-09-21 18:47:37 -0400

    Added DotNetXtensions.Test.ConsoleApp simple project: references the test project(s) to allow quicker debugging / development (F5, etc) of whatever test methods we're working on.

---
+++ Test/DotNetXtensions.Test.ConsoleApp/readme.md
@@ -0,0 +1,3 @@
+# DotNetXtensions.Test.ConsoleApp
+
+Console app that references the test project(s) to allow quicker debugging / development (F5, etc) of whatever test method we're working on.
---
+++ Test/DotNetXtensions.Test.ConsoleApp/src/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DotNetXtensions.Test;
+
+namespace DotNetXtensions.Test.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//xdictNVCollTests.TestAll();
+		}
+
+
+		//public static XDictionary_NameValueCollection_ToDictionary xdictNVCollTests {
+		//	get {
+		//		if (__xdictNVCollTests == null)
+		//			__xdictNVCollTests = new XDictionary_NameValueCollection_ToDictionary();
+		//		return __xdictNVCollTests;
+		//	}
+		//}
+		//static XDictionary_NameValueCollection_ToDictionary __xdictNVCollTests;
+
+		//public static XDateTimes_DateTimeParsing xdateTimeTests {
+		//	get {
+		//		if (__xdateTimeTests == null)
+		//			__xdateTimeTests = new XDateTimes_DateTimeParsing();
+		//		return __xdateTimeTests;
+		//	}
+		//}
+		//static XDateTimes_DateTimeParsing __xdateTimeTests;
+
+	}
+}

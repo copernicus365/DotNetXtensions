d2c9bc137e5a9eac46739a1fa6f0e3e3ac954fba
Nicholas Petersen <copernicus365@gmail.com>
2018-11-16 09:57:00 -0500

    GridOutputBuilder improvements. Multi-line content can now be kept in line with it's column with `IndentLineBreaks`. Other improvements to this type.

--- DotNetXtensions.Common2/src/Types/GridOutputBuilder.cs
+++ DotNetXtensions.Common2/src/Types/GridOutputBuilder.cs
@@ -46,43 +46,103 @@ namespace DotNetXtensions
 	/// </summary>
 	public class GridOutputBuilder
 	{
-		int _padding;
-		int _numberOfColumns = -1;
-		int[] _paddingPerColumn;
+		public int Padding { get; set; } = 1;
+
+		public bool IndentLineBreaks { get; set; } = true;
+
+		int _colCount = -1;
+
+		int[] _colPaddings; // remains null if never set
+
+		List<string> _currRow; // remains null if never set = new List<string>();
+
 		List<string[]> _lines = new List<string[]>();
 
-		public bool IsFixedColumnSize {
-			get {
-				return _paddingPerColumn.NotNulle();
-			}
-		}
 
-		public GridOutputBuilder(int padding = 1)
-		{
-			_padding = padding;
-		}
+
+		public bool IsFixedColumnSize => _colPaddings.NotNulle();
+
+		public bool AutoNewRowsOnAdd { get; set; } //= true;
+
+		public GridOutputBuilder() { }
+
+		public GridOutputBuilder(int padding) => Padding = padding;
 
 		public GridOutputBuilder(params int[] paddingPerColumn)
 		{
-			if(paddingPerColumn.IsNulle())
+			if (paddingPerColumn.IsNulle())
 				throw new ArgumentNullException();
-			_paddingPerColumn = paddingPerColumn;
-			_numberOfColumns = paddingPerColumn.Length;
+			_colPaddings = paddingPerColumn;
+			_colCount = paddingPerColumn.Length;
 		}
 
-
-		public GridOutputBuilder Add(params object[] items)
+		public GridOutputBuilder Add(object item)
 		{
-			if(items.NotNulle()) {
+			string itm = item?.ToString();
 
-				if(_numberOfColumns < 0)
-					_numberOfColumns = items.Length;
-				else if(_numberOfColumns != items.Length)
-					throw new ArgumentOutOfRangeException();
+			if (_currRow == null)
+				_currRow = new List<string>();
+			else if (_currRow.Count >= _colCount) // should NEVER happen, because see below, we check and call AddRow, and this is the only public add method
+				throw new ArgumentOutOfRangeException(); // $"Row is already filled, cannot add another item to it. Make sure to call {nameof(EndRow)} after a row is filled"); //, or to enable {nameof(AutoNewRowsOnAdd)}");
 
-				_lines.Add(items.Select(obj => obj?.ToString()).ToArray());
+			_currRow.Add(itm);
+
+			if (_currRow.Count == _colCount) {
+				string[] arr = _currRow.ToArray();
+				_currRow.Clear();
+				_AddRow(arr);
 			}
+
+			//AddRow(
+			//	_currRow.Select(s => s as object).ToArray()); // this is kindof messed, paying price of params interprets this type as a single object
+
 			return this;
+		}
+
+		/// <summary>
+		/// Ends current row if one has been started (see Add methods that allow items to be added 
+		/// via multiple calls for a single row). Is harmless when called before a row has been started.
+		/// </summary>
+		public void EndRow()
+		{
+			if (_currRow.NotNulle()) {
+				if (_colCount < 0)
+					_colCount = _currRow.Count;
+				AddRow(_currRow);
+				_currRow.Clear();
+			}
+		}
+
+		public GridOutputBuilder AddRow(params object[] items)
+			=> _AddRow(items?.Select(obj => obj?.ToString()).ToArray());
+
+		public GridOutputBuilder _AddRow(string[] items)
+		{
+			if (items.IsNulle())
+				throw new ArgumentNullException();
+			if (_currRow.NotNulle())
+				throw new Exception("Internal invalid, current row should be empty or null");
+
+			_assertRowLength(items.Length);
+			_lines.Add(items);
+
+			return this;
+		}
+
+		void _setRowLength(int rowLength)
+		{
+			if (_colCount < 0)
+				_colCount = rowLength;
+			else if (_colCount != rowLength)
+				throw new ArgumentOutOfRangeException();
+		}
+
+		void _assertRowLength(int rowLength)
+		{
+			if (_colCount < 0)
+				_colCount = rowLength;
+			else if (_colCount != rowLength)
+				throw new ArgumentOutOfRangeException();
 		}
 
 		/// <summary>
@@ -94,7 +154,7 @@ namespace DotNetXtensions
 		{
 			//if(padding != null) //int? padding = null
 			//	_padding = padding.Value;
-			_numberOfColumns = -1;
+			_colCount = -1;
 			_lines.Clear();
 			return this;
 		}
@@ -107,29 +167,51 @@ namespace DotNetXtensions
 		/// Each item in each line will be padded with the input padding size. Each array 
 		/// must contain the same number of elements (i.e. grid items - they CAN be null or empty, but need to all
 		/// be of the same size).
+		/// </summary>
 		/// <param name="lines">A list of string arrays, where each array in the list represents a row in the output grid.</param>
 		/// <param name="padding">Additional padding between each element (default = 1)</param>
-		/// </summary>
-		public static string ToGridOutputAuto(int padding, List<string[]> lines)
+		/// <param name="indentLineBreaks">True to have any line breaks within values to also be indented</param>
+		public static string ToGridOutputAuto(
+			int padding, 
+			List<string[]> lines,
+			bool indentLineBreaks)
 		{
 			int numLines = lines[0].Length;
 			var maxValues = new int[numLines];
+			var lbPads = indentLineBreaks ? new int[numLines] : null;
 
-			for(int i = 0; i < numLines; i++)
-				maxValues[i] = lines.Max(x => (x.Length > i + 1 && x[i] != null ? x[i].Length : 0)) + padding;
+			for (int i = 0; i < numLines; i++) {
+				maxValues[i] = lines.Max(x =>
+					(x.Length > i + 1 && 
+					 x[i] != null ? x[i].Length : 0)) 
+					 + padding;
+			}
+
+			if (indentLineBreaks) {
+				for (int i = 1; i < numLines; i++) {
+					lbPads[i] = maxValues.Take(i + 1).Sum();
+				}
+			}
 
 			var sb = new StringBuilder();
 
-			for(int i = 0; i < lines.Count; i++) {
+			for (int i = 0; i < lines.Count; i++) {
 
 				string[] arr = lines[i];
 
-				if(i != 0)
+				if (i != 0)
 					sb.AppendLine();
 
-				for(int j = 0; j < arr.Length; j++) {
+				for (int j = 0; j < arr.Length; j++) {
 					string value = arr[j] ?? "";
-					string paddedVal = value.PadRight(maxValues[j]);
+					int totalWidth = maxValues[j];
+
+					string paddedVal = value.PadRight(totalWidth);
+
+					if (indentLineBreaks && j > 0 && value.IndexOf('\n') > 0) {
+						paddedVal = paddedVal.Replace("\n", "\n" + new string(' ', lbPads[j]));
+					}
+
 					sb.Append(paddedVal);
 				}
 			}
@@ -138,23 +220,23 @@ namespace DotNetXtensions
 
 		public static string ToGridOutputFixed(int[] paddingPerColumn, List<string[]> lines)
 		{
-			if(paddingPerColumn.IsNulle())
+			if (paddingPerColumn.IsNulle())
 				throw new ArgumentOutOfRangeException();
 
 			int columnsLen = paddingPerColumn.Length;
 
 			var sb = new StringBuilder();
 
-			for(int i = 0; i < lines.Count; i++) {
+			for (int i = 0; i < lines.Count; i++) {
 
 				string[] arr = lines[i];
-				if(arr.Length != columnsLen)
+				if (arr.Length != columnsLen)
 					throw new ArgumentOutOfRangeException();
 
-				if(i != 0)
+				if (i != 0)
 					sb.AppendLine();
 
-				for(int j = 0; j < columnsLen; j++) {
+				for (int j = 0; j < columnsLen; j++) {
 					string value = arr[j] ?? "";
 					string paddedVal = value.PadRight(paddingPerColumn[j]);
 					sb.Append(paddedVal);
@@ -170,13 +252,13 @@ namespace DotNetXtensions
 		/// </summary>
 		public override string ToString()
 		{
-			if(_lines.IsNulle())
+			if (_lines.IsNulle())
 				return null;
 
-			if(IsFixedColumnSize)
-				return ToGridOutputFixed(_paddingPerColumn, _lines);
+			if (IsFixedColumnSize)
+				return ToGridOutputFixed(_colPaddings, _lines);
 			else
-				return ToGridOutputAuto(_padding, _lines);
+				return ToGridOutputAuto(Padding, _lines, IndentLineBreaks);
 		}
 
 	}

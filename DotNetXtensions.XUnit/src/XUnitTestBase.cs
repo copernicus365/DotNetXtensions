b2e7e383090929a57a584fcdccc2032ebcf110f6
Nicholas Petersen <copernicus365@gmail.com>
2022-03-10 23:13:35 -0500

    - big changes, added BaseDataPath for when reading not from embedded but from copied to output files. - dependency now only on xunit.assert (!)

--- DotNetXtensions.XUnit/src/XUnitTestBase.cs
+++ DotNetXtensions.XUnit/src/XUnitTestBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 using DotNetXtensions;
@@ -10,8 +11,24 @@ namespace DotNetXtensions.Test;
 
 public abstract class XUnitTestBase : EmbeddedResources
 {
-	public XUnitTestBase() { }
+	/// <summary>
+	/// Empty constructor, or to be used when setting the <see cref="BaseDataPath"/>.
+	/// See documentation on that property for more information.
+	/// </summary>
+	/// <param name="dataBasePath">See documentation on <see cref="BaseDataPath"/>.</param>
+	public XUnitTestBase(string dataBasePath = "data")
+		=> BaseDataPath = dataBasePath;
 
+	/// <summary>
+	/// Constructor to call when using embedded resources, namely when
+	/// going to be calling <see cref="EmbeddedResources.ResourceString(string, bool?)"/>,
+	/// etc.
+	/// </summary>
+	/// <param name="typeForEmbeddedResources">A type that exists in the project to read embedded resources from.</param>
+	/// <param name="resourceBasePath">If using embedded resources, should be sub-directory path
+	/// after project, separated by periods. If using resources copied to the output directory,
+	/// should be the normal relative path (slash separated) after </param>
+	/// <param name="cacheResourceGets"></param>
 	public XUnitTestBase(
 		Type typeForEmbeddedResources,
 		string resourceBasePath = null,
@@ -21,6 +38,65 @@ public abstract class XUnitTestBase : EmbeddedResources
 		if(cacheResourceGets != null)
 			CacheResourceGetsDefault = cacheResourceGets.Value;
 	}
+
+
+	#region --- DataPath ---
+
+
+	/// <summary>
+	/// When using <see cref="DataString(string)"/> and
+	/// <see cref="DataBytes(string)"/>, this path represents the path after
+	/// <see cref="Environment.CurrentDirectory"/> which to read files from.
+	/// To have this work, see example:
+	/// <para />
+	/// With a root project directory `data` exists with let's say json files that need
+	/// accessed by the test project, in csproj put within an `ItemGroup`:
+	/// <![CDATA[<Content Include="data\**\*.*"><CopyToOutputDirectory>Always</CopyToOutputDirectory></Content>]]>
+	/// </summary>
+	public string BaseDataPath {
+		get => _baseDataPath;
+		private set {
+			_baseDataPath = value.IsNulle()
+				? null
+				: value.Replace('.', '/');
+		}
+	}
+	string _baseDataPath = "data";
+
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="path"></param>
+	/// <returns></returns>
+	public string DataString(string path)
+	{
+		string fpath = _getDataPath(BaseDataPath, path);
+		string content = File.ReadAllText(fpath);
+		Assert.True(content != null);
+		return content;
+	}
+
+	public byte[] DataBytes(string path)
+	{
+		string fpath = _getDataPath(BaseDataPath, path);
+		var content = File.ReadAllBytes(fpath);
+		Assert.True(content != null);
+		return content;
+	}
+
+	static string _getDataPath(string baseDir, string path)
+	{
+		string currDir = Environment.CurrentDirectory;
+		string fpath = baseDir.IsNulle()
+			? Path.Combine(currDir, path)
+			: Path.Combine(currDir, baseDir, path);
+		return fpath;
+	}
+
+
+	#endregion
+
 
 	#region --- Proj Paths ---
 
@@ -38,6 +114,7 @@ public abstract class XUnitTestBase : EmbeddedResources
 		=> ProjectPath.ProjSrcPath(endPathAfterSrc);
 
 	#endregion
+
 
 	#region --- OutLog ---
 
